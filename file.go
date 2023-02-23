27508bd2a53a0e49ee413968eb1b45b31041223b
Boris Passerat de La Chapelle <boris@everblix.fr>
2023-02-23 09:12:03 +0100

    Add read/write from io.Reader/io.Writer

--- file.go
+++ file.go
@@ -1,6 +1,9 @@
 package bimg
 
-import "io/ioutil"
+import (
+	"io"
+	"io/ioutil"
+)
 
 // Read reads all the content of the given file path
 // and returns it as byte buffer.
@@ -12,4 +15,15 @@ func Read(path string) ([]byte, error) {
 // to the given file path.
 func Write(path string, buf []byte) error {
 	return ioutil.WriteFile(path, buf, 0644)
+}
+
+func ReadFromReader(r io.Reader) ([]byte, error) {
+	return ioutil.ReadAll(r)
+}
+
+// Write writes the given byte buffer into disk
+// to the given file path.
+func WriteToWriter(w io.Writer, buf []byte) error {
+	_, err := w.Write(buf)
+	return err
 }
